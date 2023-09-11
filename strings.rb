a6296782210d4caef552368233bb627f3ee06542
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-09-11 20:01:50 +0530

    Add files via upload

---
+++ strings.rb
@@ -0,0 +1,24 @@
+puts "Hello world!"
+
+puts "1"+"1"
+
+puts "1".to_i
+
+puts 1.to_s
+
+age = 20
+name = "David"
+puts "Hello #{name}, our records tell us that you're ${age} years old!"
+
+letters = ['a','b','c']
+puts letters[0]
+puts letters[1]
+puts letters[2]
+puts letters.size
+
+#you can add new elements to an array like this
+numbers = []
+numbers << 1
+numbers << 2
+numbers << 3
+puts numbers
